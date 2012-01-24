828fe516489fcb4b00878574960333666c0c8052
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-01-23 19:23:33 -0800

    Added rvm install

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 echo "Setting up server..."
-sudo apt-get install git
+sudo apt-get install git -y
+sudo bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
 echo $1
